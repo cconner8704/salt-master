642623a528df2f85a978235ad2cf144fbce6b5ab
Chris Conner <cconner@cloudera.com>
2018-04-04 19:58:00 -0400

    Test build

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,9 @@
 # log level
 LOG_LEVEL=${LOG_LEVEL:-"error"}
 
+while [ 1 ]
+do
+	sleep 100
+done
 # run salt as a deamon
-exec /usr/bin/salt-master --log-level=$LOG_LEVEL
+#exec /usr/bin/salt-master --log-level=$LOG_LEVEL
